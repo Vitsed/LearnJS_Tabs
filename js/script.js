81c07119a14f7991ac5a3cc7b98ba0bdbd40a5f9
Vitalii Sednev <phen1007@gmail.com>
2020-02-27 16:52:53 +0400

    Добавлена функция hideTabContent

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-window.addEventListener('DOMContentLoaded', () => {
-    'use strict'
+window.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'), 
+        tabContent = document.querySelectorAll('.info-tabcontent');
+    
+    function hideTabContent(a) {
+        for(let i = a; i < tabContent.length; i++ ) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    } 
+
+    hideTabContent(1);
+
+    console.log('test');
 });
+
+
+
